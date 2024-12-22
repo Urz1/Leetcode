643d5ae5648020cd959ef66f19a58a3c8a883beb
Sadam Husen Ali <101430982+Urz1@users.noreply.github.com>
2024-12-22 23:08:44 +0300

    Time: 15 ms (49.23%) | Memory: 18.2 MB (5.63%) - LeetSync

--- 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
+++ 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
@@ -1,15 +1,20 @@
 class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
-        tracker = {list1[i]:[i] for i in range(len(list1))}
+        tracker = {list1[i]: [i] for i in range(len(list1))}
         minIndex = float('inf')
-        Cstring = []
-        for key in range(len(list2)):
-            if list2[key] in tracker:
-                if minIndex >= sum(tracker[list2[key]]) + key:
-                    minIndex = sum(tracker[list2[key]]) + key
-                tracker[list2[key]].append(key)
-        for keys in tracker:
-            if len(tracker[keys]) > 1 and tracker[keys][0] + tracker[keys][1] == minIndex:
-                Cstring.append(keys)
-        return Cstring
-        
+        commonRestaurants = []
+
+        # Iterate through list2 to find common restaurants and calculate index sums
+        for i in range(len(list2)):
+            if list2[i] in tracker:
+                indexSum = tracker[list2[i]][0] + i
+                if indexSum <= minIndex:
+                    minIndex = indexSum
+                tracker[list2[i]].append(i)
+
+       # Collect restaurants with the smallest index sum
+        for restaurant in tracker:
+            if len(tracker[restaurant]) > 1 and sum(tracker[restaurant]) == minIndex:
+                commonRestaurants.append(restaurant)
+
+        return commonRestaurants
