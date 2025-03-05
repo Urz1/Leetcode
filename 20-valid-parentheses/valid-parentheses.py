5c06f3b11199e1f183bef81048f488b9ce40f6ff
Sadam Husen Ali <101430982+Urz1@users.noreply.github.com>
2025-03-05 13:05:42 +0000

    Time: 0 ms (100.00%) | Memory: 17.8 MB (51.05%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,13 +1,22 @@
-class Solution(object):
-    def isValid(self, s):
+class Solution:
+    def isValid(self, s: str) -> bool:
         stack = []
-        bracket_dict = {')': '(', '}': '{', ']': '['}
-
-        for char in s:
-            if char in bracket_dict.values():
-                stack.append(char)
-            elif char in bracket_dict:
-                if not stack or stack.pop() != bracket_dict[char]:
+        for ch in s:
+            if ch == ')':
+                if stack and stack.pop() == '(':
+                    continue 
+                else:
                     return False
-
-        return not stack
+            elif ch == ']':
+                if stack and stack.pop() == '[':
+                    continue
+                else:
+                    return False
+            elif ch == '}':
+                if stack and stack.pop() == '{':
+                    continue
+                else:
+                    return False
+            else:
+                stack.append(ch)
+        return True if not stack else False
