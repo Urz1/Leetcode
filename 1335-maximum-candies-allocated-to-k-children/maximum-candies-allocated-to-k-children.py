5e2b130049a3ac927c17331d2dfaab47efae4d01
Sadam Husen Ali <101430982+Urz1@users.noreply.github.com>
2025-03-29 22:46:20 +0300

    Time: 535 ms (12.27%) | Memory: 29.7 MB (85.05%) - LeetSync

---
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maximumCandies(self, candies: List[int], k: int) -> int:
+        def isValid(mid):
+            count = 0
+            for candy in candies:
+                if mid > 0:
+                    count += (candy // mid)
+            return count >= k
+        candies.sort()
+        low = 1
+        high = candies[-1]
+        ans = 0
+        while low <= high:
+            mid = (low + high) // 2
+
+            if isValid(mid):
+                ans = mid
+                low = mid + 1
+            else:
+                high = mid - 1
+        return ans
