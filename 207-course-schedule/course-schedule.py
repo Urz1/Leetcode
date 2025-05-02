996fe7f2b498eeae3a43f97a0293be64b6251d9a
Sadam Husen Ali <101430982+Urz1@users.noreply.github.com>
2025-05-02 17:32:58 +0300

    Time: 4 ms (68.60%) | Memory: 19.3 MB (40.00%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,53 +1,24 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-
-        '''
-        using coloring 
-        assume white as 0
-        gray as 1
-        black as 2
-        *** if we got at a point where we have visited it's childrens and it's color is gray 
-        this means we have got a cycle 
-        '''
-        graph = defaultdict(list)
-        for a,b in prerequisites:
-            graph[b].append(a)
-
-        colors = [0 for course in range(numCourses)]
-        def dfs(course):
-            if colors[course] == 1:
-                return False
-
-            if colors[course] == 2:
-                return True
-            colors[course] = 1
-
-            for child in graph[course]:
-                if not dfs(child):
-                    return False
-            colors[course] = 2
-
-            return True
+        flag = True
+        graph  = defaultdict(list)
+        for course, prereq in prerequisites:
+            graph[prereq].append(course)
+        visited = set()
+        colors = [0] * numCourses
+        def dfs(node):
+            nonlocal flag
+            if not flag:
+                return
+            colors[node] = 1
+            for neighbor in graph[node]:
+                if colors[neighbor] == 1:
+                    flag = False 
+                    return
+                if colors[neighbor] == 0:
+                    dfs(neighbor)
+            colors[node] = 2
         for course in range(numCourses):
-            if not dfs(course):
-                return False
-
-        return True
-
-
-        # visited = defaultdict(int)
-        # def dfs(vertex, visited):
-        #     if visited[vertex] == 1:
-        #         return False
-        #     visited[vertex] = visited.get(vertex, 0) + 1
-        #     for neighbour in graph[vertex]:
-        #         if neighbour not in visited:
-        #             dfs(neighbour, visited)
-        # graph = defaultdict(list)
-        # for course in prerequisites:
-        #     graph[course[0]].append(course[1])
-        #     # graph[course[1]].append(course[0])
-        # for course in prerequisites:
-        #     if visited[course[0]] != 2:
-        #         dfs(course[1],visited) 
-        # return True
+            if colors[course] == 0:
+                dfs(course)
+        return flag
